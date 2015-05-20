356c1f55656ab34bc3ed4d616bbc36b83fa895b2
ashalini90 <ashalini90@tamu.edu>
2015-05-20 14:09:19 -0500

    added initial traceback_utils files, changed Makefile, config.mk

---
+++ traceback/traceback_utils.h
@@ -0,0 +1,13 @@
+/** @file traceback_utils.h
+ *  @brief provides the definitions for util functions
+ *
+ *  @author Shalini Priya Ashok Kumar
+ */
+
+#ifndef __TRACEBACK_UTILS_H
+#define __TRACEBACK_UTILS_H
+
+
+void *get_frame_pointer();
+
+#endif /* __TRACEBACK_UTILS_H */
